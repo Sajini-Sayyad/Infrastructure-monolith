2d53cc9fb1d3cabd48327ceb80d032bcc9f8ee77
sajini <sajini.syi@gmail.com>
2026-08-13 15:48:50 +0530

    Allow inbound port 8090 in NSG for web application

--- main.tf
+++ main.tf
@@ -98,6 +98,20 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "allow-app-8090"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+
+    source_port_range          = "*"
+    destination_port_range     = "8090"
+
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # -----------------------------
